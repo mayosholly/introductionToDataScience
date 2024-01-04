97539005837e9f987be52483bc2f1762402fcd94
mayosholly <mayosholly@gmail.com>
2024-01-04 02:54:07 +0100

    introduction to data science

---
+++ Crash Course/crash.py
@@ -0,0 +1,15 @@
+# Introduction to Data Science
+# What is Data?
+# Python for Data Science
+# Python Pandas
+# Python Numpy
+# Python Scikit-learn
+# Python Matplotlib
+
+# Python Basics
+
+# Data Processing
+
+# Data Visualization
+
+# Machine Learning
---
+++ Lesson Four/codes/pandas.py
@@ -0,0 +1,221 @@
+# Creating a Pandas DataFrame
+# import pandas as pd
+import pandas as pd
+ 
+# list of strings
+lst = ['Geeks', 'For', 'Geeks', 'is', 
+            'portal', 'for', 'Geeks']
+ 
+# Calling DataFrame constructor on list
+df = pd.DataFrame(lst)
+print(df)
+
+
+
+# Creating DataFrame from dict of ndarray/lists:
+# Python code demonstrate creating 
+# DataFrame from dict narray / lists 
+# By default addresses.
+ 
+import pandas as pd
+ 
+# intialise data of lists.
+data = {'Name':['Tom', 'nick', 'krish', 'jack'],
+        'Age':[20, 21, 19, 18]}
+ 
+# Create DataFrame
+df = pd.DataFrame(data)
+ 
+# Print the output.
+print(df)
+
+
+# Dealing with Rows and Columns
+# Import pandas package
+import pandas as pd
+ 
+# Define a dictionary containing employee data
+data = {'Name':['Jai', 'Princi', 'Gaurav', 'Anuj'],
+        'Age':[27, 24, 22, 32],
+        'Address':['Delhi', 'Kanpur', 'Allahabad', 'Kannauj'],
+        'Qualification':['Msc', 'MA', 'MCA', 'Phd']}
+ 
+# Convert the dictionary into DataFrame 
+df = pd.DataFrame(data)
+ 
+# select two columns
+print(df[['Name', 'Qualification']])
+
+
+# Indexing and Selecting Data
+# importing pandas package
+import pandas as pd
+ 
+# making data frame from csv file
+data = pd.read_csv("nba.csv", index_col ="Name")
+ 
+# retrieving columns by indexing operator
+first = data["Age"]
+ 
+ 
+ 
+print(first)
+
+
+
+# Selecting a single row
+# importing pandas package
+import pandas as pd
+ 
+# making data frame from csv file
+data = pd.read_csv("nba.csv", index_col ="Name")
+ 
+# retrieving row by loc method
+first = data.loc["Avery Bradley"]
+second = data.loc["R.J. Hunter"]
+ 
+ 
+print(first, "\n\n\n", second)
+
+
+
+# Selecting a single row
+import pandas as pd
+ 
+# making data frame from csv file
+data = pd.read_csv("nba.csv", index_col ="Name")
+ 
+ 
+# retrieving rows by iloc method 
+row2 = data.iloc[3] 
+ 
+ 
+ 
+print(row2)
+
+
+# Working with Missing Data
+# Checking for missing values using isnull() and notnull() :
+# importing pandas as pd
+import pandas as pd
+ 
+# importing numpy as np
+import numpy as np
+ 
+# dictionary of lists
+dict = {'First Score':[100, 90, np.nan, 95],
+        'Second Score': [30, 45, 56, np.nan],
+        'Third Score':[np.nan, 40, 80, 98]}
+ 
+# creating a dataframe from list
+df = pd.DataFrame(dict)
+ 
+# using isnull() function  
+df.isnull()
+
+
+# Filling missing values using fillna(), replace() and interpolate() :
+# importing pandas as pd
+import pandas as pd
+ 
+# importing numpy as np
+import numpy as np
+ 
+# dictionary of lists
+dict = {'First Score':[100, 90, np.nan, 95],
+        'Second Score': [30, 45, 56, np.nan],
+        'Third Score':[np.nan, 40, 80, 98]}
+ 
+# creating a dataframe from dictionary
+df = pd.DataFrame(dict)
+ 
+# filling missing value using fillna()  
+df.fillna(0)
+
+
+
+# Dropping missing values using dropna() :
+# importing pandas as pd
+import pandas as pd
+ 
+# importing numpy as np
+import numpy as np
+ 
+# dictionary of lists
+dict = {'First Score':[100, 90, np.nan, 95],
+        'Second Score': [30, np.nan, 45, 56],
+        'Third Score':[52, 40, 80, 98],
+        'Fourth Score':[np.nan, np.nan, np.nan, 65]}
+ 
+# creating a dataframe from dictionary
+df = pd.DataFrame(dict)
+   
+df
+
+
+# /Now we drop rows with at least one Nan value (Null value)
+
+# importing pandas as pd
+import pandas as pd
+ 
+# importing numpy as np
+import numpy as np
+ 
+# dictionary of lists
+dict = {'First Score':[100, 90, np.nan, 95],
+        'Second Score': [30, np.nan, 45, 56],
+        'Third Score':[52, 40, 80, 98],
+        'Fourth Score':[np.nan, np.nan, np.nan, 65]}
+ 
+# creating a dataframe from dictionary
+df = pd.DataFrame(dict)
+ 
+# using dropna() function  
+df.dropna()
+
+
+
+# Iterating over rows and columns
+# importing pandas as pd
+import pandas as pd
+  
+# dictionary of lists
+dict = {'name':["aparna", "pankaj", "sudhir", "Geeku"],
+        'degree': ["MBA", "BCA", "M.Tech", "MBA"],
+        'score':[90, 40, 80, 98]}
+ 
+# creating a dataframe from a dictionary 
+df = pd.DataFrame(dict)
+ 
+print(df)
+
+
+
+# importing pandas as pd
+import pandas as pd
+  
+# dictionary of lists
+dict = {'name':["aparna", "pankaj", "sudhir", "Geeku"],
+        'degree': ["MBA", "BCA", "M.Tech", "MBA"],
+        'score':[90, 40, 80, 98]}
+ 
+# creating a dataframe from a dictionary 
+df = pd.DataFrame(dict)
+ 
+print(df)
+
+
+
+# Iterating over Columns :
+# importing pandas as pd
+import pandas as pd
+   
+# dictionary of lists
+dict = {'name':["aparna", "pankaj", "sudhir", "Geeku"],
+        'degree': ["MBA", "BCA", "M.Tech", "MBA"],
+        'score':[90, 40, 80, 98]}
+  
+# creating a dataframe from a dictionary 
+df = pd.DataFrame(dict)
+ 
+print(df)
